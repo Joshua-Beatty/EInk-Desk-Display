9dc3a1aa8d5a22fb40711d675b5c71849d592f1e
Joshua Beatty <joshuatb6@gmail.com>
2025-02-02 11:48:03 -0700

    test

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ epd.init()
 epd.Clear()
 
 
+print("Drawing image")
 with open("test.png", "rb") as f:
     buffer = io.BytesIO(f.read())
     epd.display(buffer)
 
 
+print("Image Drawn")
 time.sleep(2)
 epd.sleep()
