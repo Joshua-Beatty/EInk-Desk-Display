5c7f0a89fe9aba9f4295ce2b225501363fb816fc
Joshua Beatty <joshuatb6@gmail.com>
2025-02-02 12:12:45 -0700

    updating

--- main.py
+++ main.py
@@ -11,26 +11,18 @@ epd.Clear()
 
 
 print("Drawing image")
+with Image.open("test1.png") as img:
+    img = img.convert("1")
+    img = ImageOps.invert(img)
+    buffer = img.tobytes() 
+    epd.display(buffer)
+time.sleep(2)
 with Image.open("test.png") as img:
     img = img.convert("1")
     img = ImageOps.invert(img)
     buffer = img.tobytes() 
     epd.display(buffer)
 
-# partial update
-print("5.show time")
-epd.init_part()
-Himage = Image.new('1', (epd.width, epd.height), 0)
-draw = ImageDraw.Draw(Himage)
-num = 0
-while (True):
-    draw.rectangle((31, 150, 130, 170), fill = 255)
-    draw.text((31, 150), time.strftime('%H:%M:%S'), fill = 0)
-    Himage = ImageOps.invert(Himage)
-    epd.display_Partial(epd.getbuffer(Himage),0, 0, epd.width, epd.height)
-    num = num + 1
-    if(num == 10):
-        break
 
 
 
