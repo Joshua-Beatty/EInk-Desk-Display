b4822d92fb5976891a0a51b12dbf343c9211bb1f
Joshua Beatty <joshuatb6@gmail.com>
2025-02-02 14:44:56 -0700

    udpating

--- index.ts
+++ index.ts
@@ -1,33 +1,34 @@
 import sharp from "sharp";
 import EPDController from "./epdController";
-import fs from "fs"
+import fs from "fs";
 
+async function drawTime(epd: EPDController) {
+  const image = sharp("./test2.png");
 
-async function drawTime(epd: EPDController){
-    const image = sharp("./test2.png");
-
-    const time = new Date().toLocaleString().split(", ")[1];
-    await image
-      .composite([
-        {
-          input: Buffer.from(
-            `<svg>
+  const time = new Date().toLocaleString().split(", ")[1];
+  await image
+    .composite([
+      {
+        input: Buffer.from(
+          `<svg>
                 <rect x="0" y="0" width="450" height="150" fill="#fff" />
                 <text x="430" y="130" text-anchor="end" font-size="74" fill="#000">${time}</text>
             </svg>`
-          ),
-          top: 480 - 150,
-          left: 800 - 450,
-          gravity: "southeast"
-        },
-      ])
-      .toFile("./output/test.png");
-    await epd.drawPartial("./output/test.png", 0, 0, 800, 480);
-    console.log("Partial update completed");
+        ),
+        top: 480 - 150,
+        left: 800 - 450,
+        gravity: "southeast",
+      },
+    ])
+    .toFile("./output/test.png");
+  await epd.drawPartial("./output/test.png", 0, 0, 800, 480);
+  console.log("Partial update completed");
 }
 async function main() {
   const epd = new EPDController();
-  fs.mkdirSync('./output');
+  try {
+    fs.mkdirSync("./output");
+  } catch (e) {}
 
   try {
     // Execute commands sequentially with proper typing
@@ -40,21 +41,21 @@ async function main() {
     await epd.drawPartial("./test2.png", 0, 0, 800, 480);
     console.log("Partial update completed");
 
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
-    await drawTime(epd)
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
+    await drawTime(epd);
 
     await epd.sleep();
     console.log("Display sleeping");
