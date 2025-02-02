d973ba861c5f653be22ef7e423bc796430004010
Joshua Beatty <joshuatb6@gmail.com>
2025-02-02 12:16:30 -0700

    updating

--- main.py
+++ main.py
@@ -11,20 +11,22 @@ epd.Clear()
 
 
 print("Drawing image")
-with Image.open("test1.png") as img:
-    img = img.convert("1")
-    img = ImageOps.invert(img)
-    buffer = img.tobytes() 
+with Image.open("test1.png") as Himage:
+    Himage = Himage.convert("1")
+    Himage = ImageOps.invert(Himage)
+    buffer = Himage.tobytes() 
     epd.display(buffer)
-time.sleep(2)
-with Image.open("test.png") as img:
-    img = img.convert("1")
-    img = ImageOps.invert(img)
-    buffer = img.tobytes() 
-    epd.display(buffer)
-
-
-
+    print("5.show time")
+    epd.init_part()
+    draw = ImageDraw.Draw(Himage)
+    num = 0
+    while (True):
+        draw.rectangle((31, 150, 130, 170), fill = 255)
+        draw.text((31, 150), time.strftime('%H:%M:%S'), fill = 0)
+        epd.display_Partial(epd.getbuffer(Himage),0, 0, epd.width, epd.height)
+        num = num + 1
+        if(num == 10):
+            break
 
 print("Image Drawn")
 time.sleep(2)
