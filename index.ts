4ed19e5241dd68e427a22816fcdfe807818b7db1
Joshua Beatty <joshuatb6@gmail.com>
2025-02-02 14:19:42 -0700

    etsting

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import sharp from "sharp";
 import EPDController from "./epdController";
 
 async function main() {
@@ -12,6 +13,26 @@ async function main() {
     console.log("Full image drawn");
 
     await epd.drawPartial("./test2.png", 0, 0, 800, 480);
+    console.log("Partial update completed");
+
+    const image = sharp("./test2.png");
+
+    await image
+      .composite([
+        {
+          input: {
+            text: {
+              text: new Date().toLocaleString().split(", ")[1],
+              width: 450,
+              height: 150,
+            },
+          },
+          top: 480 - 150,
+          left: 800 - 450,
+        },
+      ])
+      .toFile("./test.png");
+    await epd.drawPartial("./test.png", 0, 0, 800, 480);
     console.log("Partial update completed");
 
     await epd.sleep();
